59022a5c55541fd47dfbd0307d00333b3a2944dc
CreativeWrec <58211428+CreativeWrec@users.noreply.github.com>
2020-04-24 10:28:50 -0500

    Intial_Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fun_With_For_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        public Program()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
